95d6e786bdb0bfe508e905ae8b5d17a22927c18c
angelajm6 <angelajaume.m@gmail.com>
2023-03-24 10:30:12 -0400

    initial index.js setup

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+
 const app = express();
+const PORT = 3001;
+
 const mongoose = require ("mongoose");
 const dotenv = require ("dotenv");
 
@@ -8,3 +13,15 @@ dotenv.config();
 app.listen(1300, () => {
     console.log("App is now running")
 })
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
