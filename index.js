3be1ea6cc8904579cdab6cdd0ed2d77bc90315e9
angelajm6 <angelajaume.m@gmail.com>
2023-03-26 00:23:44 -0400

    created MongoDB connection

--- index.js
+++ index.js
@@ -14,14 +14,17 @@ app.listen(1300, () => {
     console.log("App is now running")
 })
 
+mongoose.connect('mongodb://127.0.0.1:27017/myapp'); {
+    console.log ("connected to MondoDB")
+};
 
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(routes);
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
-});
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//   });
+// });
